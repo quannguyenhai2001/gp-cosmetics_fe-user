3840fa00d1f0e2c14affebeccf4568535d584041
quannguyenhai2001 <quanbn134679258@gmail.com>
2023-03-16 01:32:19 +0700

    add navbar UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
 import { Box } from '@mui/material';
+import Navbar from './Navbar/Navbar';
 export default function Header() {
 
     return (
         <Box>
-            Header
+            <Navbar />
         </Box >
     );
 }
