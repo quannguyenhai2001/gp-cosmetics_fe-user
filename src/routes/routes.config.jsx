c60c14faeecded3c50190d33bb851f1c5bcefd96
quannguyenhai2001 <quanbn134679258@gmail.com>
2023-03-17 17:41:03 +0700

    add signup UI

--- src/routes/routes.config.jsx
+++ src/routes/routes.config.jsx
@@ -1,10 +1,17 @@
 import DefaultLayout from "layouts/DefaultLayout/DefaultLayout";
 import HomeScreen from "screens/HomeScreen/HomeScreen";
+import SignUpUserScreen from "screens/SignUpSreen/SignUpSreen";
 
 export const RouteConfigs = [
     {
         path: "/",
         element: HomeScreen,
+        isPrivate: false,
+        layout: DefaultLayout,
+    },
+    {
+        path: "/sign-up",
+        element: SignUpUserScreen,
         isPrivate: false,
         layout: DefaultLayout,
     }
